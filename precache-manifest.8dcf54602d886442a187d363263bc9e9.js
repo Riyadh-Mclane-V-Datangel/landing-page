8d42be2f240e3eb440918c25852176c052d08022
mclane <mclane0618@gmail.com>
2024-05-15 15:13:31 +0800

    Updates

---
+++ precache-manifest.8dcf54602d886442a187d363263bc9e9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e5514fea78d32a6a7bb85e50d2e78e",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "8bc1525759255c0ca51c",
+    "url": "/login/static/css/main.b10b22c0.chunk.css"
+  },
+  {
+    "revision": "698932391d445e6e7d80",
+    "url": "/login/static/js/2.ff446d7b.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/login/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "8bc1525759255c0ca51c",
+    "url": "/login/static/js/main.09c59da7.chunk.js"
+  },
+  {
+    "revision": "9adfbab25f57c3362922",
+    "url": "/login/static/js/runtime~main.80c013e7.js"
+  },
+  {
+    "revision": "474e04370124e5f5e6c18e6f5ce5f112",
+    "url": "/login/static/media/1.474e0437.jpg"
+  },
+  {
+    "revision": "4eed9fac83cec6b08f1e22f3717c0b46",
+    "url": "/login/static/media/2.4eed9fac.jpg"
+  },
+  {
+    "revision": "4f443eafa11aa2cf50ae9058d762da88",
+    "url": "/login/static/media/3.4f443eaf.jpg"
+  },
+  {
+    "revision": "38a75e02b04a1b0523f58aa532120786",
+    "url": "/login/static/media/IbarraRealNova-Medium.38a75e02.ttf"
+  },
+  {
+    "revision": "b4ab32bca9dae366fa7193b1b7bb1b4c",
+    "url": "/login/static/media/InterTight-Medium.b4ab32bc.ttf"
+  },
+  {
+    "revision": "bdb7ba651b7bdcda6ce527b3b6705334",
+    "url": "/login/static/media/Montserrat-Medium.bdb7ba65.ttf"
+  },
+  {
+    "revision": "7b32ffdeb5f62ccd26fd57fa9430dbb7",
+    "url": "/login/static/media/blur.7b32ffde.png"
+  },
+  {
+    "revision": "45c849e0552dadc1364e3ddb1dd1013a",
+    "url": "/login/static/media/build.45c849e0.jpg"
+  },
+  {
+    "revision": "581d78958350f7dfc38a7aa5bad5aa9f",
+    "url": "/login/static/media/build2.581d7895.jpg"
+  },
+  {
+    "revision": "5290b12d7068574f5dd6c338a8416aef",
+    "url": "/login/static/media/fb.5290b12d.png"
+  },
+  {
+    "revision": "c03d209208afb884b6e2ed3453a40471",
+    "url": "/login/static/media/insta.c03d2092.png"
+  },
+  {
+    "revision": "e9925c5e0e7df439bcd4414186bd17a3",
+    "url": "/login/static/media/irislogo.e9925c5e.png"
+  },
+  {
+    "revision": "03b15fdc8db86060e3a84a63490c2928",
+    "url": "/login/static/media/twitter.03b15fdc.png"
+  }
+]);
